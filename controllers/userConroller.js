f5d61234c4f6593412f0fb6098ff422278ed9623
Dmitry Sychev <dimtrisythev@gmail.com>
2023-02-02 21:25:22 +0600

    feature: added login function

--- controllers/userConroller.js
+++ controllers/userConroller.js
@@ -12,15 +12,49 @@ const UserDto = require("../dtos/UserDto");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const uuid = require("uuid");
+const { validationResult } = require("express-validator");
 
 class UserController {
-  //TODO: Сделать функцию для проверки токенов
-  async check(req, res) {
-    res.json({ message: "fwfewfwgwg" });
+  async login(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(ApiError.badRequest("Invalid body"));
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user === null) {
+      return next(ApiError.notFound("User not found"));
+    }
+
+    const isPassworValid = await bcrypt.compare(password, user.password);
+    if (!isPassworValid) {
+      return next(ApiError.badRequest("Invalid invalid password"));
+    }
+
+    const dto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...dto });
+    await tokenService.saveToken(user.id, tokens.refreshToken);
+
+    res.cookie("refreshToken", tokens.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json({
+      ...tokens,
+      user: dto,
+    });
   }
 
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.badRequest("Invalid body"));
+      }
+
       const { email, name, password } = req.body;
 
       const candidate = await User.findOne({ where: { email } });
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,10 +1,21 @@
 const Router = require("express");
 const router = new Router();
+const { body } = require("express-validator");
 
 const userController = require("../controllers/userConroller");
 
-router.post("/registration", userController.registration);
-router.post("/login");
-router.get("/auth", userController.check);
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 32 }),
+  body("name").exists().not().isEmpty(),
+  userController.registration
+);
+router.post(
+  "/login",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 32 }),
+  userController.login
+);
 
 module.exports = router;
